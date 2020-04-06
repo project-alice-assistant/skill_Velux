41449f5b86a461276f247a5e3a640a77090fa483
Psycho <laurentchervet@bluewin.ch>
2020-04-06 21:37:26 +0200

    Working base

--- Velux.py
+++ Velux.py
@@ -1,6 +1,8 @@
-	from core.base.model.AliceSkill import AliceSkill
+import threading
+
+from core.base.model.AliceSkill import AliceSkill
 from core.dialog.model.DialogSession import DialogSession
-from core.util.Decorators import IntentHandler
+from core.util.Decorators import IntentHandler, MqttHandler
 
 
 class Velux(AliceSkill):
@@ -9,19 +11,43 @@ class Velux(AliceSkill):
 	Description: Control your velux equipment with your voice! This requires a velux remote hack
 	"""
 
+	def __init__(self):
+		super().__init__()
+		self._waiting = threading.Event()
+
+
+	@MqttHandler('projectalice/devices/velux/state')
+	def veluxState(self, session: DialogSession):
+		if session.payload.get('state', '') == 1:
+			self._waiting.clear()
+		elif not self._waiting.is_set():
+			self._waiting.set()
+
+
 	@IntentHandler('Velux_DoAction')
 	def handleVelux(self, session: DialogSession, **_kwargs):
-		room = session.slotValue(slotName='Room', defaultValue=session.siteId)
-		action = session.slotValue(slotName='Action', defaultValue='open')
-		device = session.slotValue(slotName='Device', defaultValue='windows')
-		duration = session.slotValue(slotName='Duration', defaultValue=-1)
-		percentage = session.slotValue(slotName='Percentage', defaultValue=100)
+		if self._waiting.is_set():
+			self.endDialog(
+				sessionId=session.sessionId,
+				text=self.randomTalk(text='willDoLater')
+			)
+		else:
+			room = session.slotValue(slotName='Room', defaultValue=session.siteId)
+			action = session.slotValue(slotName='Action', defaultValue='open')
+			device = session.slotValue(slotName='Device', defaultValue='windows')
+			duration = session.slotValue(slotName='Duration', defaultValue=-1)
+			percentage = session.slotValue(slotName='Percentage', defaultValue=100)
 
-		self.publish(
-			topic=f'projectalice/devices/{device}/{action}',
-			payload = {
-				'room'      : room,
-				'duration'  : duration,
-				'percentage': percentage
-			}
-		)
+			self.publish(
+				topic=f'projectalice/devices/{device}/{action}',
+				payload = {
+					'room'      : room,
+					'duration'  : duration,
+					'percentage': percentage
+				}
+			)
+
+			self.endDialog(
+				sessionId=session.sessionId,
+				text=self.randomTalk(text=f'ok', replace=[])
+			)
